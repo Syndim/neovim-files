54d397a5b520877cef31972c4c6a2dcdca319f06
Syndim <im@syndim.org>
2021-04-24 08:42:10 +0800

    Fix toggel term settings

--- lua/plugins/_toggleterm.lua
+++ lua/plugins/_toggleterm.lua
@@ -7,7 +7,7 @@ function M.config()
         hide_numbers = true, -- hide the number column in toggleterm buffers
         shade_filetypes = {},
         shade_terminals = true,
-        shading_factor = '<number>', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+        shading_factor = '1', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
         start_in_insert = true,
         persist_size = true,
         direction = 'float',
