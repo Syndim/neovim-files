233b8acc4628d836e87306bf647c8486e591ffd2
Syndim <im@syndim.org>
2021-06-26 19:10:04 +0800

    Update config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -9,7 +9,7 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+-- vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(
     {
