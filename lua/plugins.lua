17d20bc9d56878bb1a804540613d345ecfdfb158
Syndim <im@syndim.org>
2021-12-01 21:07:35 +0800

    Refine packer initialization code

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,7 +4,7 @@ local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+  execute('!git clone --depth=1 https://ghproxy.com/https://github.com/wbthomason/packer.nvim '..install_path)
   execute 'packadd packer.nvim'
 end
 
