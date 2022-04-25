cc1ef35953473573ee4fde47c13c0b95e03ddb11
Syndim <im@syndim.org>
2022-04-25 20:26:14 +0800

    Fix autocmd trigger

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,3 +1,3 @@
 local api = vim.api
 
-api.nvim_create_autocmd({ 'VimEnter', 'BufWinEnter', 'BufRead', 'BufNewFile' }, { pattern = { '*.arb' }, command = [[setlocal filetype=json conceallevel=0]] })
+api.nvim_create_autocmd({ 'VimEnter', 'BufWinEnter', 'BufRead', 'BufNewFile', 'BufEnter' }, { pattern = { '*.arb' }, command = [[setlocal filetype=json conceallevel=0]] })
