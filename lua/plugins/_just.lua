1f230d1b7fcaeb999e6458cea19e59d9bfe0a8c7
Syndim <im@syndim.org>
2022-04-25 22:23:17 +0800

    Update just settings

--- lua/plugins/_just.lua
+++ lua/plugins/_just.lua
@@ -2,7 +2,12 @@ local M = {}
 
 function M.config()
     require('tree-sitter-just').setup({})
-    require("nvim-treesitter.install").ensure_installed({ 'just' })
+    local global = require('global')
+    local install = require("nvim-treesitter.install")
+    if global.is_mac then
+        install.compilers = { 'clang' }
+    end
+    install.ensure_installed({ 'just' })
 end
 
 return M
