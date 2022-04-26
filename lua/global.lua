40506f2185bee586c9b98233f3bc53ded7ab8281
Syndim <im@syndim.org>
2022-04-26 12:08:23 +0800

    Add neovide settings

--- lua/global.lua
+++ lua/global.lua
@@ -1,10 +1,13 @@
 local global = {}
-local os_name = vim.loop.os_uname().sysname
+local os_name = vim.loop.os_uname()
+local system = os_name.sysname
+local release = os_name.release
 
 function global:load_variables()
-  self.is_mac     = string.find(os_name, 'Darwin') ~= nil
-  self.is_linux   = string.find(os_name, 'Linux') ~= nil
-  self.is_windows = string.find(os_name, 'Windows') ~= nil
+    self.is_mac     = string.find(system, 'Darwin') ~= nil
+    self.is_linux   = string.find(system, 'Linux') ~= nil
+    self.is_windows = string.find(system, 'Windows') ~= nil
+    self.is_wsl     = string.find(release, 'WSL') ~= nil
 end
 
 global:load_variables()
---
+++ lua/neovide.lua
@@ -0,0 +1,8 @@
+if not vim.g.neovide then
+    return
+end
+
+local global = require('global')
+if global.is_wsl then
+    o.guifont = 'FiraCode NF:h14'
+end
