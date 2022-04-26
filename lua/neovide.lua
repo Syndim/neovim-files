5c152febafcfdadc75cd895fdfda0bb86c8248fb
Syndim <im@syndim.org>
2022-04-26 23:04:17 +0800

    Setup keymap for neovide on mac

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,5 +1,6 @@
 local g = vim.g
 local o = vim.o
+local api = vim.api
 if not g.neovide then
     return
 end
@@ -9,4 +10,14 @@ g.neovide_remember_window_size = true
 local global = require('global')
 if global.is_wsl then
     o.guifont = 'FiraCode NF:h14'
+end
+
+if global.is_mac then
+    local opts = {
+        noremap = true
+    }
+    api.nvim_set_keymap('n', '˙', '<C-w>h', opts)
+    api.nvim_set_keymap('n', '∆', '<C-w>j', opts)
+    api.nvim_set_keymap('n', '˚', '<C-w>k', opts)
+    api.nvim_set_keymap('n', '¬', '<C-w>l', opts)
 end
