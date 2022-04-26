e06231f77bf854745749a7bb616b90f4d614759f
Syndim <im@syndim.org>
2022-04-26 14:05:21 +0800

    Try to disable json conceal

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,3 +1,3 @@
 local api = vim.api
 
-api.nvim_create_autocmd({ 'VimEnter', 'BufWinEnter', 'BufRead', 'BufNewFile', 'BufEnter' }, { pattern = { '*.arb' }, command = [[setlocal filetype=json conceallevel=0]] })
+api.nvim_create_autocmd({ 'VimEnter', 'BufWinEnter', 'BufRead', 'BufNewFile', 'BufEnter' }, { pattern = { '*.arb' }, command = [[set filetype=json g:vim_json_conceal=0]] })
