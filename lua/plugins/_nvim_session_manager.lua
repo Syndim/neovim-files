73ab59e9aa6768c7c03c62637a17428581a395c9
Syndim <im@syndim.org>
2022-05-18 16:51:52 +0800

    Load current dir session by default

--- lua/plugins/_nvim_session_manager.lua
+++ lua/plugins/_nvim_session_manager.lua
@@ -1,7 +1,9 @@
 local M = {}
 
 function M.config()
-    require('session_manager').setup({})
+    require('session_manager').setup({
+        autoload_mode = require('session_manager.config').AutoloadMode.CurrentDir
+    })
 end
 
 return M
