9128f13e18a25992f2d483b6efcfbd442892abf9
Syndim <im@syndim.org>
2022-05-25 16:25:11 +0800

    Fix error

--- lua/plugins/_lsp_installer.lua
+++ lua/plugins/_lsp_installer.lua
@@ -102,8 +102,8 @@ function M.config()
                 config['handlers'] = {}
             end
             local default_on_attach = lsp.create_on_attach()
-            local function on_attach()
-                default_on_attach()
+            local function on_attach(client, bufnr)
+                default_on_attach(client, bufnr)
                 vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua require("omnisharp_extended").telescope_lsp_definitions()<CR>', { noremap = true, silent = true })
             end
 
