65a827c46b7b34779d0c3e9e58c4fa6eac34f058
Syndim <im@syndim.org>
2022-08-01 22:13:50 +0800

    Add pair setting for rust

--- lua/plugins/_treesitter_autopair.lua
+++ lua/plugins/_treesitter_autopair.lua
@@ -1,14 +1,25 @@
 local M = {}
 
 function M.config()
-    require('nvim-autopairs').setup({})
+    local Rule = require('nvim-autopairs.rule')
+    local npairs = require('nvim-autopairs')
+    local cond = require('nvim-autopairs.conds')
+    local ts_conds = require('nvim-autopairs.ts-conds')
+    npairs.setup({})
+    npairs.add_rules({
+        Rule('<', '>', 'rust')
+            :with_pair(ts_conds.is_ts_node({
+                'type_identifier',
+                'let_declaration',
+                'parameters',
+                'function_item',
+                'return_type',
+            }))
+            :with_cr(cond.none()),
+    })
     local cmp_autopairs = require('nvim-autopairs.completion.cmp')
     local cmp = require('cmp')
     cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
-
-    -- add a lisp filetype (wrap my-function), FYI: Hardcoded = { "clojure", "clojurescript", "fennel", "janet" }
-    -- cmp_autopairs.lisp[#cmp_autopairs.lisp+1] = "racket"
 end
 
 return M
-
