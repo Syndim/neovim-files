dc56b04c25b19a069d87580eed2bdc749d759b4d
Syndim <im@syndim.org>
2022-09-08 13:22:24 +0800

    Don't notify empty messages

--- lua/plugins/_notify.lua
+++ lua/plugins/_notify.lua
@@ -1,7 +1,13 @@
 local M = {}
 
 function M.config()
-    vim.notify = require("notify")
+    local notify_fn = require('notify')
+    vim.notify = function(msg, level, opts)
+        if string.len(msg) > 0 then
+            notify_fn(msg, level, opts)
+        end
+    end
 end
 
 return M
+
