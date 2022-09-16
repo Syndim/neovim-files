7d2a6d939787c810d0bfc32f38ca1e05d27e8f23
Syndim <im@syndim.org>
2022-09-16 15:36:21 -0700

    Update guifont

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,15 +1,16 @@
 local api = vim.api
 local global = require('global')
 
-api.nvim_create_autocmd({ 'VimEnter', 'BufWinEnter', 'BufRead', 'BufNewFile', 'BufEnter' }, { pattern = { '*.arb', '*.json' }, callback = function()
-    vim.cmd([[set filetype=json]])
-    vim.g.indentLine_conceallevel = 0
-end })
+api.nvim_create_autocmd({ 'VimEnter', 'BufWinEnter', 'BufRead', 'BufNewFile', 'BufEnter' },
+    { pattern = { '*.arb', '*.json' }, callback = function()
+        vim.cmd([[set filetype=json]])
+        vim.g.indentLine_conceallevel = 0
+    end })
 api.nvim_create_autocmd({ 'BufLeave' }, { pattern = { '*.arb', '*.json' }, callback = function()
     vim.g.indentLine_conceallevel = nil
 end })
 api.nvim_create_autocmd({ 'UIEnter' }, { callback = function()
     if global.is_wsl and vim.g.neovide then
-        vim.o.guifont = 'FiraCode NF:h14'
+        vim.o.guifont = 'FiraCode NFM:h14'
     end
 end })
