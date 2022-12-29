23e91fcd033999611a7b47a1d0fbb64fa125bf2a
Syndim <im@syndim.org>
2022-12-29 14:41:46 +0800

    Show relative number in neo tree

--- lua/plugins/_neo_tree.lua
+++ lua/plugins/_neo_tree.lua
@@ -1,23 +1,30 @@
 local M = {}
 
 function M.config()
-    vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
-    vim.fn.sign_define("DiagnosticSignError",
-        { text = " ", texthl = "DiagnosticSignError" })
-    vim.fn.sign_define("DiagnosticSignWarn",
-        { text = " ", texthl = "DiagnosticSignWarn" })
-    vim.fn.sign_define("DiagnosticSignInfo",
-        { text = " ", texthl = "DiagnosticSignInfo" })
-    vim.fn.sign_define("DiagnosticSignHint",
-        { text = "", texthl = "DiagnosticSignHint" })
+    vim.fn.sign_define('DiagnosticSignError',
+        { text = ' ', texthl = 'DiagnosticSignError' })
+    vim.fn.sign_define('DiagnosticSignWarn',
+        { text = ' ', texthl = 'DiagnosticSignWarn' })
+    vim.fn.sign_define('DiagnosticSignInfo',
+        { text = ' ', texthl = 'DiagnosticSignInfo' })
+    vim.fn.sign_define('DiagnosticSignHint',
+        { text = '', texthl = 'DiagnosticSignHint' })
 
-    require("neo-tree").setup({
+    require('neo-tree').setup({
         window = {
             mappings = {
-                ["o"] = "open",
+                ['o'] = 'open',
             },
             width = 30
-        }
+        },
+        event_handlers = {
+            {
+                event = 'neo_tree_buffer_enter',
+                handler = function(_arg)
+                    vim.cmd('setlocal rnu')
+                end,
+            }
+        },
     })
 
     local opts = {
