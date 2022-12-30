c7caec176a34c9aefa7f00bcb3a245c0f1a467f1
Syndim <im@syndim.org>
2022-12-30 21:52:43 +0800

    Work around rust tool crash

--- lua/plugins/_lsp_rust.lua
+++ lua/plugins/_lsp_rust.lua
@@ -2,7 +2,14 @@ local M = {}
 
 function M.setup(config)
     local rust_config = vim.tbl_deep_extend('force', config, {
-        standalone = true
+        standalone = true,
+
+        -- TODO: Remove this settings below after issue in rust-tool has been fixed: https://github.com/simrat39/rust-tools.nvim/issues/300
+        settings = {
+            ["rust-analyzer"] = {
+                inlayHints = { locationLinks = false },
+            },
+        },
     })
 
     require('rust-tools').setup({
@@ -11,3 +18,4 @@ function M.setup(config)
 end
 
 return M
+
