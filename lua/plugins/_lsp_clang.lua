923763ed8de858dc6bf413e00a52da85fdf86a9e
Syndim <im@syndim.org>
2023-01-30 12:47:35 +0800

    Update clang offsetEncoding

--- lua/plugins/_lsp_clang.lua
+++ lua/plugins/_lsp_clang.lua
@@ -1,8 +1,13 @@
 local M = {}
 
 function M.setup(config)
+    local clang_config = vim.tbl_deep_extend('force', config, {
+        capabilities = {
+            offsetEncoding = "utf-8"
+        }
+    })
     require('clangd_extensions').setup({
-        server = config
+        server = clang_config
     })
 end
 
