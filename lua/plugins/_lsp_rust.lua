ea2c876d874b7f2c04b65510fda5450bf6f5d378
Syndim <im@syndim.org>
2023-03-09 15:09:59 +0800

    Remove workaround for rust-tools

--- lua/plugins/_lsp_rust.lua
+++ lua/plugins/_lsp_rust.lua
@@ -3,13 +3,6 @@ local M = {}
 function M.setup(config)
     local rust_config = vim.tbl_deep_extend('force', config, {
         standalone = true,
-
-        -- TODO: Remove this settings below after issue in rust-tool has been fixed: https://github.com/simrat39/rust-tools.nvim/issues/300
-        settings = {
-            ["rust-analyzer"] = {
-                inlayHints = { locationLinks = false },
-            },
-        },
     })
 
     require('rust-tools').setup({
@@ -18,4 +11,3 @@ function M.setup(config)
 end
 
 return M
-
