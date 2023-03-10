7b32356c42ccb1f8cc9cb7e25b1f63ae9a4dcf20
Syndim <im@syndim.org>
2023-03-10 15:30:16 +0800

    Enable proc macro for rust-analyzer

--- lua/plugins/_lsp_rust.lua
+++ lua/plugins/_lsp_rust.lua
@@ -10,11 +10,14 @@ function M.setup(config)
         settings = {
             -- to enable rust-analyzer settings visit:
             -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-                ['rust-analyzer'] = {
+            ['rust-analyzer'] = {
                 -- enable clippy on save
                 checkOnSave = {
                     command = 'clippy'
                 },
+                procMacro = {
+                    enable = true
+                }
             }
         }
     })
