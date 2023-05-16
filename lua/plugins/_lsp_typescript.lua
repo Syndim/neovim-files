dd019feb30074521c02f8bcde0feab5ed80f6255
Syndim <im@syndim.org>
2023-05-16 08:32:45 +0800

    Fix tsserver lsp key binding issue

--- lua/plugins/_lsp_typescript.lua
+++ lua/plugins/_lsp_typescript.lua
@@ -2,8 +2,8 @@ local M = {}
 
 function M.setup(lsp_config, config)
     local ts_config = vim.tbl_deep_extend('force', config, {
-        on_attach = function(client)
-            config.on_attach(client)
+        on_attach = function(client, bufnr)
+            config.on_attach(client, bufnr)
             client.server_capabilities.documentFormattingProvider = false
             client.server_capabilities.documentRangeFormattingProvider = false
         end
