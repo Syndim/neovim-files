cab134c60aac1a1834fdc6fe0f1518b0d8738ccd
Syndim <im@syndim.org>
2023-07-13 10:32:22 +0800

    Disable install lint items for now as null-ls is removed

--- lua/plugins/_mason_tool_installer.lua
+++ lua/plugins/_mason_tool_installer.lua
@@ -3,10 +3,10 @@ local M = {}
 function M.config()
     require('mason-tool-installer').setup({
         ensure_installed = {
-            'ruff',
-            'hadolint',
-            'eslint_d',
-            'prettierd'
+            -- 'ruff',
+            -- 'hadolint',
+            -- 'eslint_d',
+            -- 'prettierd'
         }
     })
 end
