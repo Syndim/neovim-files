99dbc44ed9b525960fd855be8d5331a43f7513ba
Syndim <im@syndim.org>
2024-01-14 22:39:09 +0800

    Fix typo in features.lua

--- lua/features.lua
+++ lua/features.lua
@@ -9,17 +9,17 @@ local M = {
     format_on_save_enabled = true,
     lsp_config = {},
 
-    is_coilot_enabled = false,
+    is_copilot_enabled = false,
     is_codeium_enabled = false,
 
     enable_copilot = function()
         load_plugins({ 'copilot.lua', 'copilot-status.nvim' })
-        require('features').is_coilot_enabled = true
+        require('features').is_copilot_enabled = true
     end,
 
     enable_codeium = function()
         load_plugins({ 'codeium.vim' })
-        require('features').is_coilot_enabled = true
+        require('features').is_codeium_enabled = true
     end
 }
 
