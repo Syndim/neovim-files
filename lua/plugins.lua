b5452ea7a3461a7d36102a729ec7133fbe99021f
Syndim <im@syndim.org>
2024-01-29 19:07:23 +0800

    Disable codeium version due to start up issue

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -500,6 +500,7 @@ require('lazy').setup(
             cmd = 'Codeium',
             config = require('plugins._codeium').config,
             init = require('plugins._codeium').setup,
+            version = false,
             cond = is_not_embedded
         },
 
