7dd176d9bfc86d8fa5aba772e07fa2f6624ecacd
Syndim <im@syndim.org>
2024-01-29 20:51:31 +0800

    Make lsp key binding remappable

--- lua/plugins/_lsp.lua
+++ lua/plugins/_lsp.lua
@@ -71,7 +71,7 @@ end
 function M.create_config()
     -- Mappings.
     -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-    local opts = { remap = false }
+    local opts = { remap = true }
     vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
     vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
     vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
