1bd13570ad0a46b9456a86523dd1ab6d7f50b761
Syndim <im@syndim.org>
2024-09-02 17:20:53 +0800

    Refine minimap config

--- lua/plugins/_minimap.lua
+++ lua/plugins/_minimap.lua
@@ -1,14 +1,21 @@
 local M = {}
 
 function M.setup()
+    local minimap_width = 15
     vim.g.neominimap = {
         float = {
-            minimap_width = 15
+            minimap_width = minimap_width
         },
         split = {
-            minimap_width = 15
+            minimap_width = minimap_width
+        },
+        click = {
+            enabled = true
         }
     }
+
+    vim.o.wrap = false
+    vim.o.sidescrolloff = minimap_width + 5
 end
 
 return M
