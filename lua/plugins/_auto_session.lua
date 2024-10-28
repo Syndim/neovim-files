35857d3e4ff78c32da865d23c9d86b307e37e34f
Syndim <im@syndim.org>
2024-10-28 08:12:11 +0000

    Remove some options from session

--- lua/plugins/_auto_session.lua
+++ lua/plugins/_auto_session.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.config()
-    vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
+    vim.o.sessionoptions = 'blank,buffers,curdir,help,tabpages,winsize,winpos,terminal'
     require('auto-session').setup({
         pre_save_cmds = { 'NvimTreeClose' }
     })
