fb73692700136cc1452e55c9edd59d962d77b611
Syndim <im@syndim.org>
2024-11-12 20:52:00 +0800

    Fix dotnet root env for neovide under WSL2

--- lua/gui/neovide.lua
+++ lua/gui/neovide.lua
@@ -1,11 +1,11 @@
 local g = vim.g
 if not g.neovide then
-    return
+	return
 end
 
 g.neovide_remember_window_size = true
 
-local global = require('global')
+local global = require("global")
 
 -- api.nvim_create_autocmd({ 'UIEnter' }, {
 --     callback = function()
@@ -16,21 +16,24 @@ local global = require('global')
 -- })
 
 if global.is_mac then
-    vim.g.neovide_input_macos_option_key_is_meta = 'both'
+	vim.g.neovide_input_macos_option_key_is_meta = "both"
 elseif global.is_wsl then
-    -- Neovide under wsl will fetch new set of environments and VIRTUAL_ENV
-    -- environment variable somehow get lost
-    -- So we reset it here
-    local util = require('lspconfig/util')
-    local path = util.path
-    local cwd = vim.fn.getcwd()
-    local match = vim.fn.glob(path.join(cwd, 'poetry.lock'))
-    if match ~= '' then
-        local venv = vim.fn.trim(vim.fn.system('poetry env info -p'))
-        vim.env.VIRTUAL_ENV = venv
-        vim.env.PATH = path.join(venv, 'bin') .. ':' .. vim.env.PATH
-    end
+	-- Neovide under wsl will fetch new set of environments and VIRTUAL_ENV
+	-- environment variable somehow get lost
+	-- So we reset it here
+	local util = require("lspconfig/util")
+	local path = util.path
+	local fn = vim.fn
+	local cwd = fn.getcwd()
+	local match = fn.glob(path.join(cwd, "poetry.lock"))
+	if match ~= "" then
+		local venv = fn.trim(vim.fn.system("poetry env info -p"))
+		vim.env.VIRTUAL_ENV = venv
+		vim.env.PATH = path.join(venv, "bin") .. ":" .. vim.env.PATH
+	end
 
-    local node_path = vim.fn.trim(vim.fn.system('mise where node'))
-    vim.env.PATH = path.join(node_path, 'bin') .. ':' .. vim.env.PATH
+	local node_path = fn.trim(vim.fn.system("volta which node"))
+	vim.env.PATH = path.dirname(node_path) .. ":" .. vim.env.PATH
+	local dotnet_root = fn.trim(fn.system("brew --prefix dotnet"))
+	vim.env.DOTNET_ROOT = path.join(dotnet_root, "libexec")
 end
