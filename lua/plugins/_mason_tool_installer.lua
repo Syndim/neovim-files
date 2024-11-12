b0c60953616c6ce8bf26faa4da2653d6deb7117d
Syndim <im@syndim.org>
2024-11-12 20:45:11 +0800

    Add lua and cs formatter

--- lua/plugins/_mason_tool_installer.lua
+++ lua/plugins/_mason_tool_installer.lua
@@ -1,16 +1,18 @@
 local M = {}
 
 function M.config()
-    require('mason-tool-installer').setup({
-        ensure_installed = {
-            'prettierd',
-            'yamlfmt',
-            'rust-analyzer',
-            'roslyn'
-        }
-    })
+	require("mason-tool-installer").setup({
+		ensure_installed = {
+			"prettierd",
+			"yamlfmt",
+			"rust-analyzer",
+			"roslyn",
+			"stylua",
+			"csharpier",
+		},
+	})
 
-    vim.cmd.MasonToolsInstall()
+	vim.cmd.MasonToolsInstall()
 end
 
 return M
