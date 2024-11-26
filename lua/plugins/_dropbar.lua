13fc9057c72a8c038b6347f624511974c0c41e5d
Syndim <im@syndim.org>
2024-11-26 12:53:33 +0800

    Disable treesitter source for dropbar in C++

--- lua/plugins/_dropbar.lua
+++ lua/plugins/_dropbar.lua
@@ -1,7 +1,40 @@
 local M = {}
 
 function M.config()
-	require("dropbar").setup({})
+    require("dropbar").setup({
+        bar = {
+            sources = function(buf, _)
+                local sources = require("dropbar.sources")
+                local utils = require("dropbar.utils")
+                if vim.bo[buf].ft == "markdown" then
+                    return {
+                        sources.path,
+                        sources.markdown,
+                    }
+                end
+                if vim.bo[buf].ft =="cpp" then
+                    return {
+                        sources.path,
+                        utils.source.fallback({
+                            sources.lsp,
+                        })
+                    }
+                end
+                if vim.bo[buf].buftype == "terminal" then
+                    return {
+                        sources.terminal,
+                    }
+                end
+                return {
+                    sources.path,
+                    utils.source.fallback({
+                        sources.lsp,
+                        sources.treesitter,
+                    }),
+                }
+            end,
+        },
+    })
 end
 
 return M
