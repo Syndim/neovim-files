507953cba21ea4d41de310bc50282ea4ce652347
Syndim <im@syndim.org>
2024-11-27 02:14:37 +0000

    Disable treesitter in dropbar for java

--- lua/plugins/_dropbar.lua
+++ lua/plugins/_dropbar.lua
@@ -1,40 +1,41 @@
 local M = {}
 
 function M.config()
-    require("dropbar").setup({
-        bar = {
-            sources = function(buf, _)
-                local sources = require("dropbar.sources")
-                local utils = require("dropbar.utils")
-                if vim.bo[buf].ft == "markdown" then
-                    return {
-                        sources.path,
-                        sources.markdown,
-                    }
-                end
-                if vim.bo[buf].ft =="cpp" then
-                    return {
-                        sources.path,
-                        utils.source.fallback({
-                            sources.lsp,
-                        })
-                    }
-                end
-                if vim.bo[buf].buftype == "terminal" then
-                    return {
-                        sources.terminal,
-                    }
-                end
-                return {
-                    sources.path,
-                    utils.source.fallback({
-                        sources.lsp,
-                        sources.treesitter,
-                    }),
-                }
-            end,
-        },
-    })
+	require("dropbar").setup({
+		bar = {
+			sources = function(buf, _)
+				local sources = require("dropbar.sources")
+				local utils = require("dropbar.utils")
+				local ft = vim.bo[buf].ft
+				if ft == "markdown" then
+					return {
+						sources.path,
+						sources.markdown,
+					}
+				end
+				if ft == "cpp" or ft == "java" then
+					return {
+						sources.path,
+						utils.source.fallback({
+							sources.lsp,
+						}),
+					}
+				end
+				if vim.bo[buf].buftype == "terminal" then
+					return {
+						sources.terminal,
+					}
+				end
+				return {
+					sources.path,
+					utils.source.fallback({
+						sources.lsp,
+						sources.treesitter,
+					}),
+				}
+			end,
+		},
+	})
 end
 
 return M
