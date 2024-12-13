70c6d2c53c6371b04e8cac6120598ebe7a35bab4
Syndim <im@syndim.org>
2024-12-13 18:04:46 +0800

    Append parameters to shell under Windows

--- lua/basic.lua
+++ lua/basic.lua
@@ -63,7 +63,12 @@ end
 g.mapleader = ","
 
 if global.is_windows then
-	o.shell = "powershell.exe"
+	local home = os.getenv("HOME")
+	if home then
+		o.shell = "powershell.exe -NoExit -File " .. home .. "/.config/powershell/setup/setup.ps1"
+	else
+		o.shell = "powershell.exe"
+	end
 elseif global.is_linux or global.is_mac then
 	o.shell = "fish"
 end
