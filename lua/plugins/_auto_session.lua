5c11b7349334faf6fe8b63dad582131fac90ccdd
Syndim <im@syndim.org>
2024-12-17 09:48:25 +0000

    Close outline before save session

--- lua/plugins/_auto_session.lua
+++ lua/plugins/_auto_session.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.config()
 	vim.o.sessionoptions = "blank,buffers,curdir,help,tabpages,winsize,winpos,terminal"
 	require("auto-session").setup({
-		pre_save_cmds = { "NvimTreeClose" },
+		pre_save_cmds = { "OutlineClose" },
 	})
 end
 
