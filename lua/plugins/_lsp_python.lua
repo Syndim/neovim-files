bac7ea20e1e472c3278fe6da60b053727eefaa16
Syndim <im@syndim.org>
2024-12-22 22:06:49 +0800

    Fix python lsp settings

--- lua/plugins/_lsp_python.lua
+++ lua/plugins/_lsp_python.lua
@@ -4,25 +4,26 @@ function M.setup(lsp_config, config)
 	-- lsp_config['pylsp'].setup(config)
 
 	-- Below config are for pyright
-	local util = require("lspconfig/util")
 	local global = require("global")
-	local path = util.path
+	local Path = require("plenary.path")
 	-- print(vim.env.VIRTUAL_ENV)
 
 	local function update_venv_path(venv)
 		vim.env.VIRTUAL_ENV = venv
+		local venv_path = Path:new(venv)
 		if global.is_windows then
-			vim.env.PATH = path.join(venv, "Scripts") .. ";" .. vim.env.PATH
+			vim.env.PATH = tostring(venv_path / "Scripts") .. ";" .. vim.env.PATH
 		else
-			vim.env.PATH = path.join(venv, "bin") .. ":" .. vim.env.PATH
+			vim.env.PATH = tostring(venv_path / "bin") .. ":" .. vim.env.PATH
 		end
 	end
 
 	local function format_python_path(venv)
+		local venv_path = Path:new(venv)
 		if global.is_windows then
-			return path.join(venv, "Scripts", "python")
+			return tostring(venv_path / "Scripts" / "python")
 		else
-			return path.join(venv, "bin", "python")
+			return tostring(venv_path / "bin" / "python")
 		end
 	end
 
@@ -33,33 +34,35 @@ function M.setup(lsp_config, config)
 			return format_python_path(vim.env.VIRTUAL_ENV)
 		end
 
-		-- Find and use venv from poetry
-		local match = vim.fn.glob(path.join(workspace, "poetry.lock"))
-		if match ~= "" then
-			local venv = vim.fn.trim(vim.fn.system("poetry env info -p"))
-			update_venv_path(venv)
-			return format_python_path(venv)
-		end
+		local workspace_path = Path:new(workspace)
 
-		match = vim.fn.glob(path.join(workspace, "pyproject.toml"))
-		if match ~= "" then
-			local rye_show_output = vim.fn.trim(vim.fn.system("rye show"))
-			local _, venv_start_index = string.find(rye_show_output, "venv: ")
-			local venv_end_index = string.find(rye_show_output, "\n", venv_start_index + 1)
-			local venv = vim.fn.trim(string.sub(rye_show_output, venv_start_index, venv_end_index))
-			update_venv_path(venv)
-			return format_python_path(venv)
-		end
+		-- Find and use venv from poetry
+		-- local match = vim.fn.glob(tostring(workspace_path / "poetry.lock"))
+		-- if match ~= "" then
+		-- 	local venv = vim.fn.trim(vim.fn.system("poetry env info -p"))
+		-- 	update_venv_path(venv)
+		-- 	return format_python_path(venv)
+		-- end
+
+		-- match = vim.fn.glob(tostring(workspace_path / "pyproject.toml"))
+		-- if match ~= "" then
+		-- 	local rye_show_output = vim.fn.trim(vim.fn.system("rye show"))
+		-- 	local _, venv_start_index = string.find(rye_show_output, "venv: ")
+		-- 	local venv_end_index = string.find(rye_show_output, "\n", venv_start_index + 1)
+		-- 	local venv = vim.fn.trim(string.sub(rye_show_output, venv_start_index, venv_end_index))
+		-- 	update_venv_path(venv)
+		-- 	return format_python_path(venv)
+		-- end
 
 		-- Find and use virtualenv from pipenv in workspace directory.
-		match = vim.fn.glob(path.join(workspace, "Pipfile"))
-		if match ~= "" then
-			local venv = vim.fn.trim(vim.fn.system("PIPENV_PIPFILE=" .. match .. " pipenv --venv"))
-			return format_python_path(venv)
-		end
+		-- match = vim.fn.glob(tostring(workspace_path / "Pipfile"))
+		-- if match ~= "" then
+		-- 	local venv = vim.fn.trim(vim.fn.system("PIPENV_PIPFILE=" .. match .. " pipenv --venv"))
+		-- 	return format_python_path(venv)
+		-- end
 
 		-- Find .venv folder
-		local dot_venv_path = path.join(workspace, ".venv")
+		local dot_venv_path = tostring(workspace_path / ".venv")
 		if vim.fn.isdirectory(dot_venv_path) == 1 then
 			update_venv_path(dot_venv_path)
 			return format_python_path(dot_venv_path)
