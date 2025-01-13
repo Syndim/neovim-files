9e688252fe2fabc957f2311bb9b9878c665d9bd7
Syndim <im@syndim.org>
2025-01-13 11:03:30 +0800

    Hack avante is_available

--- lua/plugins/_avante.lua
+++ lua/plugins/_avante.lua
@@ -8,6 +8,19 @@ function M.config()
 			provider = "telescope",
 		},
 	})
+
+	-- TODO: Remove this hack after this PR https://github.com/yetone/avante.nvim/pull/864
+	local registry = require("blink.compat.registry")
+	local original_registre_source = registry.register_source
+	function registry.register_source(name, s)
+		if string.find(name, "avante_", 1, true) then
+			function s:is_available()
+				return vim.bo.filetype == "AvanteInput"
+			end
+		end
+
+		original_registre_source(name, s)
+	end
 end
 
 return M
