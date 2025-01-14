fdb9e82c11c880daef19d588a2eef733486be5ba
Syndim <im@syndim.org>
2025-01-14 11:02:55 +0800

    Refine shortcuts to avoid conflict

--- lua/plugins/_cpp_tools.lua
+++ lua/plugins/_cpp_tools.lua
@@ -10,13 +10,13 @@ function M.config()
 
 	vim.keymap.set(
 		{ "n", "v" },
-		"<Leader>cdc",
+		"<Leader>cgc",
 		vim.cmd.TSCppDefineClassFunc,
 		{ remap = false, desc = "Generate class definition" }
 	)
 	vim.keymap.set(
 		{ "n", "v" },
-		"<Leader>cmc",
+		"<Leader>cgi",
 		vim.cmd.TSCppMakeConcreteClass,
 		{ remap = false, desc = "Generate concrete class from interface" }
 	)
