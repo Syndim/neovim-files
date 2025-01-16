5a69d028aa3d222e663dbdb8c5db6d7c1e8d484f
Syndim <im@syndim.org>
2025-01-16 10:57:02 +0800

    Change the way to call command

--- lua/plugins/_clangd.lua
+++ lua/plugins/_clangd.lua
@@ -1,7 +1,9 @@
 local M = {}
 
 function M.config()
-	vim.api.nvim_create_user_command("A", vim.cmd.ClangdSwitchSourceHeader, { nargs = 0 })
+	vim.api.nvim_create_user_command("A", function()
+		vim.cmd.ClangdSwitchSourceHeader()
+	end, { nargs = 0 })
 end
 
 return M
