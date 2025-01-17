4565b4dda332481f81fd8f4c9fe4d37ce44b2e30
Syndim <im@syndim.org>
2025-01-17 13:45:10 +0800

    Disable auto_suggestions setting for avantee

--- lua/plugins/_avante.lua
+++ lua/plugins/_avante.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.config()
 	require("avante").setup({
 		provider = "copilot",
-		auto_suggestions_provider = "copilot",
+		-- auto_suggestions_provider = "copilot",
 		file_selector = {
 			provider = "telescope",
 		},
