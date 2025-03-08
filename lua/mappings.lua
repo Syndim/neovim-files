d26fb8fc073306c1980a0883675f23dcd798610b
Syndim <im@syndim.org>
2025-03-08 21:27:17 +0800

    Config cleanup

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,5 +1,3 @@
-local api = vim.api
-
 local opts = {
 	remap = false,
 }
@@ -13,16 +11,9 @@ vim.keymap.set("n", "<C-left>", "<C-O>", opts)
 vim.keymap.set("n", "<C-right>", "<C-I>", opts)
 vim.keymap.set("n", "<Leader>b", "<C-O>", opts)
 vim.keymap.set("n", "<Leader>n", "<C-I>", opts)
--- Neovim 0.7 separates tab and c-i so we need to map both
--- vim.keymap.set('n', '<Tab>', ':bp<CR>', opts)
--- vim.keymap.set('n', '<C-I>', ':bp<CR>', opts)
--- vim.keymap.set('n', '<C-O>', ':bn<CR>', opts)
-
--- Close buffer
--- vim.keymap.set('n', '<Leader>x', ':Bdelete<CR>', { noremap = true, nowait = true })
 
 -- Clear CRLF
--- vim.keymap.set('n', '<Leader>r', ':%s/\\r//g<CR>', {})
+vim.keymap.set("n", "<Leader>r", "<cmd>%s/\\r//g<CR>", {})
 
 -- Copy/Paste/Cut
 vim.keymap.set("n", "YY", '"+y<CR>', opts)
@@ -49,7 +40,7 @@ vim.keymap.set("n", "<Leader>ch", vim.cmd.noh, opts)
 
 -- Close all buffers
 opts.desc = "Close all buffers"
-vim.keymap.set("n", "<Leader>X", ":%bd<CR>", opts)
+vim.keymap.set("n", "<Leader>X", "<cmd>%bd<CR>", opts)
 
 vim.keymap.set("n", "<C-h>", "gcc", { remap = true })
 vim.keymap.set("v", "<C-h>", "gc", { remap = true })
