eafd5d1d716d85fee27b4084d7463dbcc3742294
Syndim <im@syndim.org>
2025-03-17 17:06:49 +0800

    Close spectre before saving sessions

--- lua/plugins/_auto_session.lua
+++ lua/plugins/_auto_session.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.config()
 	vim.o.sessionoptions = "blank,buffers,curdir,help,tabpages,winsize,winpos,terminal"
 	require("auto-session").setup({
-		pre_save_cmds = { "OutlineClose", "SnacksExplorerClose" },
+		pre_save_cmds = { "OutlineClose", "SnacksExplorerClose", "SpectreClose" },
 	})
 end
 
--- lua/plugins/_nvim_spectre.lua
+++ lua/plugins/_nvim_spectre.lua
@@ -6,6 +6,7 @@ function M.config()
 	}
 	opts.desc = "Open Spectre"
 	vim.keymap.set("n", "<leader>s", require("spectre").open, opts)
+	vim.api.nvim_create_user_command("SpectreClose", require("spectre").close, { nargs = 0 })
 end
 
 return M
