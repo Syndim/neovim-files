1628e99a10217920a97b465f5d29088d741b9c16
Syndim <im@syndim.org>
2025-03-29 22:57:14 +0800

    Clean up unused items

--- lua/plugins/_mason_lspconfig.lua
+++ lua/plugins/_mason_lspconfig.lua
@@ -42,43 +42,6 @@ function M.config()
 	})
 
 	local mason_registry = require("mason-registry")
-	-- Patch assets
-	-- if global.is_windows then
-	--     local taplo_assets = mason_registry.get_package('taplo').spec.source.asset
-	--     table.insert(taplo_assets, {
-	--         target = 'win_x64',
-	--         file = 'taplo-full-windows-x86_64.zip',
-	--         bin = 'taplo.exe'
-	--     })
-	-- end
-
-	-- local efm_spec = mason_registry.get_package('efm').spec
-	-- efm_spec.source = {
-	--     asset = {
-	--         {
-	--             target = 'win_x64',
-	--             file = 'efm-langserver_{{version}}_windows_amd64.zip',
-	--             bin = 'efm-langserver_{{version}}_windows_amd64/efm-langserver.exe'
-	--         },
-	--         {
-	--             target = 'linux_x64',
-	--             file = 'efm-langserver_{{version}}_linux_amd64.tar.gz',
-	--             bin = 'efm-langserver_{{version}}_linux_amd64/efm-langserver'
-	--         },
-	--         {
-	--             target = 'darwin_arm64',
-	--             file = 'efm-langserver_{{version}}_darwin_arm64.zip',
-	--             bin = 'efm-langserver_{{version}}_darwin_arm64/efm-langserver'
-	--         },
-	--         {
-	--             target = 'darwin_x64',
-	--             file = 'efm-langserver_{{version}}_darwin_amd64.zip',
-	--             bin = 'efm-langserver_{{version}}_darwin_amd64/efm-langserver'
-	--         }
-	--     },
-	--     id = efm_spec.source.id:replace('pkg:golang/github.com', 'pkg:github')
-	-- }
-	-- efm_spec.bin['efm-langserver'] = '{{source.asset.bin}}'
 
 	-- Use proxy for schema file
 	mason_registry:on(
@@ -105,8 +68,7 @@ function M.config()
 	require("plugins._lsp_lua").setup(lsp_config, config)
 	require("plugins._lsp_clang").setup(lsp_config, config)
 	require("plugins._lsp_python").setup(lsp_config, config)
-	-- require('plugins._lsp_typescript').setup(lsp_config, config)
-	-- require('plugins._lsp_slint').setup(config)
+	-- require("plugins._lsp_typescript").setup(lsp_config, config)
 
 	if global:which("flutter") == 0 or global:which("fvm") == 0 then
 		require("plugins._lsp_flutter").setup(lsp, config)
