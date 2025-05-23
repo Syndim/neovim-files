e6b55ddf20fe38c383324ff6f228669d34ae5c6e
Syndim <im@syndim.org>
2025-05-23 13:51:31 +0800

    Remove nvim-spider

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -173,14 +173,14 @@ require("lazy").setup({
 	},
 
 	--  Use the w, e, b motions like a spider. Move by subwords and skip insignificant punctuation.
-	{
-		"chrisgrieser/nvim-spider",
-		keys = {
-			{ "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
-			{ "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
-			{ "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
-		},
-	},
+	-- {
+	-- 	"chrisgrieser/nvim-spider",
+	-- 	keys = {
+	-- 		{ "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
+	-- 		{ "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
+	-- 		{ "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
+	-- 	},
+	-- },
 
 	-- Navigate your code with search labels, enhanced character motions and Treesitter integration
 	{
