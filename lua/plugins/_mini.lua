7558a20be7734efe703ebe15dad3f39aaf81f7e9
Syndim <im@syndim.org>
2025-06-08 16:32:04 +0800

    Enable mini.diff

--- lua/plugins/_mini.lua
+++ lua/plugins/_mini.lua
@@ -50,7 +50,7 @@ function M.config()
 	})
 
 	-- mini.diff
-	-- require("mini.diff").setup()
+	require("mini.diff").setup()
 
 	-- mini.move
 	-- Alt+h/l/j/k to move line in normal mode or block in visual mode
