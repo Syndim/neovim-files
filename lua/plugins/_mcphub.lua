0e6bb60867c74b9238b01315fe185c2b7f6d84b3
Syndim <im@syndim.org>
2025-06-25 21:57:26 +0800

    Auto approve neovim mcp tools

--- lua/plugins/_mcphub.lua
+++ lua/plugins/_mcphub.lua
@@ -3,6 +3,21 @@ local M = {}
 function M.config()
     require("mcphub").setup({
         use_bundled_binary = true,
+        auto_approve = function(params)
+            if vim.g.codecompanion_auto_tool_mode == true then
+                return true -- Auto approve when CodeCompanion auto-tool mode is on
+            end
+
+            if params.server_name == "neovim" then
+                print(params.server_name)
+                return true
+            end
+
+            if params.is_auto_approved_in_server then
+                return true -- Respect servers.json configuration
+            end
+            return false
+        end,
         -- extentions = {
         -- 	avante = {
         -- 		make_slash_commands = true,
