168bc09df931b11f4e4099fa4f3041dd2789e7a1
Syndim <im@syndim.org>
2026-01-03 10:03:44 +0800

    Update spectre settings

--- lua/plugins/_nvim_spectre.lua
+++ lua/plugins/_nvim_spectre.lua
@@ -1,12 +1,15 @@
 local M = {}
 
 function M.config()
+    local spectre = require("spectre")
+    spectre.setup({})
+
     local opts = {
         remap = false,
     }
     opts.desc = "Open Spectre"
-    vim.keymap.set("n", "<leader>s", require("spectre").open, opts)
-    vim.api.nvim_create_user_command("SpectreClose", require("spectre").close, { nargs = 0 })
+    vim.keymap.set("n", "<leader>s", spectre.open, opts)
+    vim.api.nvim_create_user_command("SpectreClose", spectre.close, { nargs = 0 })
 end
 
 return M
