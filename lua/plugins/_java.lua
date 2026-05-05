498c724859835f369109dd75cd47a89e9e1d67f4
Syndim <im@syndim.org>
2026-05-05 13:31:11 +0800

    Setup java for JDK17

--- lua/plugins/_java.lua
+++ lua/plugins/_java.lua
@@ -1,7 +1,16 @@
 local M = {}
 
 function M.config()
-    require("java").setup()
+    require("java").setup({
+        jdtls = {
+            -- Match the active Java 17 runtime; newer nvim-java defaults require Java 21+.
+            version = "1.43.0",
+        },
+        jdk = {
+            -- Avoid blocking Neovim while nvim-java downloads a full JDK on first Java file open.
+            auto_install = false,
+        },
+    })
     vim.lsp.enable("jdtls")
 end
 
