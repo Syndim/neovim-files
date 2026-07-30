357ac7e9df34dec019aabeed2a16ce15128aca21
Syndim <im@syndim.org>
2026-07-30 10:18:12 +0800

    Fix swift LSP issue

--- lua/plugins/_lsp_sourcekit.lua
+++ lua/plugins/_lsp_sourcekit.lua
@@ -3,11 +3,13 @@ local M = {}
 function M.setup(config)
     local util = require("lspconfig.util")
     local sourcekit_config = vim.tbl_deep_extend("force", config, {
-        root_dir = function(filename, _)
-            return util.root_pattern("buildServer.json")(filename)
+        root_dir = function(bufnr, on_dir)
+            local filename = vim.api.nvim_buf_get_name(bufnr)
+            local root = util.root_pattern("buildServer.json")(filename)
                 or util.root_pattern("*.xcodeproj", "*.xcworkspace")(filename)
                 or vim.fs.dirname(vim.fs.find(".git", { path = filename, upward = true })[1])
                 or util.root_pattern("Package.swift")(filename)
+            on_dir(root)
         end,
         filetypes = { "swift" },
     })
